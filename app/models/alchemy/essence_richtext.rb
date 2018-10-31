aafa189edddfed056fcbdf5c63476a9ffa337c84
Ahmad Varoqua <ahmad.varoqua@gmail.com>
2018-10-31 15:55:06 -0700

    ARAI > QA  > Database Upgrades > Fixing issues with Alchemy CMS and strip_tags

--- app/models/alchemy/essence_richtext.rb
+++ app/models/alchemy/essence_richtext.rb
@@ -20,28 +20,30 @@ module Alchemy
 
   private
 
+    # REF: https://github.com/AlchemyCMS/alchemy_cms/commit/4c07fc5240d7772eeee451bef8f39988bf9b49e8
     def strip_content
-      self.stripped_body = strip_tags(self.body)
+      # self.stripped_body = strip_tags(self.body)
+      self.stripped_body = Rails::Html::FullSanitizer.new.sanitize(body)
     end
 
-    # Stripping HTML Tags and only returns plain text.
-    def strip_tags(html)
-      return html if html.blank?
-      if html.index("<")
-        text = ""
-        tokenizer = ::HTML::Tokenizer.new(html)
-        while token = tokenizer.next
-          node = ::HTML::Node.parse(nil, 0, 0, token, false)
-          # result is only the content of any Text nodes
-          text << node.to_s if node.class == ::HTML::Text
-        end
-        # strip any comments, and if they have a newline at the end (ie. line with
-        # only a comment) strip that too
-        text.gsub(/<!--(.*?)-->[\n]?/m, "")
-      else
-        html # already plain text
-      end
-    end
+    ## Stripping HTML Tags and only returns plain text.
+    #def strip_tags(html)
+    #  return html if html.blank?
+    #  if html.index("<")
+    #    text = ""
+    #    tokenizer = ::HTML::Tokenizer.new(html)
+    #    while token = tokenizer.next
+    #      node = ::HTML::Node.parse(nil, 0, 0, token, false)
+    #      # result is only the content of any Text nodes
+    #      text << node.to_s if node.class == ::HTML::Text
+    #    end
+    #    # strip any comments, and if they have a newline at the end (ie. line with
+    #    # only a comment) strip that too
+    #    text.gsub(/<!--(.*?)-->[\n]?/m, "")
+    #  else
+    #    html # already plain text
+    #  end
+    #end
 
   end
 end
